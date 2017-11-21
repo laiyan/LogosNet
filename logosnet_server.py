b1066da903e1e8a9300cfa131542cb68c4d3a890
yungl <yungl@wwu.edu>
2017-11-21 00:47:39 -0800

    updated

--- logosnet_server.py
+++ logosnet_server.py
@@ -3,6 +3,15 @@ import socket               # Import socket module
 import os#  Low level modules for threading and handling signals
 import signal
 import struct
+import collections
+
+#object Participant
+class Participant:
+    n = 0
+    usernane = " "
+    message = " "
+
+
 
 #argument parsing
 PARSER = argparse.ArgumentParser(
@@ -11,14 +20,21 @@ PARSER.add_argument("--port", type=int, metavar='p', help="port number")
 PARSER.add_argument("--ip", metavar='i', help="IP address for client")
 ARGS = PARSER.parse_args()
 
+def chat():
+    plist = collections.deque(maxlen = 255)
+    get_username()
+
 #username method
 def get_username():
     '''for inputing the username.'''
     inputusername = input("Enter username, max 10 chars: ")
-    while len(inputusername) > 10 or inputusername.isalpha() == 0:
+    while len(inputusername) > 10 or " " in inputusername:
         inputusername = input("Enter username, max 10 chars: ")
     print(inputusername)
     return inputusername
+
+#check username exist or not
+
 
 
 # server socket setup and connection

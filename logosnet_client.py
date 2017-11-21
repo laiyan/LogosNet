492e850c5327f343f49aa213c46d9b91cd7e3ee9
yungl <yungl@wwu.edu>
2017-11-21 01:23:15 -0800

    update

--- logosnet_client.py
+++ logosnet_client.py
@@ -10,4 +10,9 @@ PARSER.add_argument("--port", type=int, metavar='p', help="port number")
 PARSER.add_argument("--ip", metavar='i', help="IP address for client")
 ARGS = PARSER.parse_args()
 
+S = socket.socket()         # Create a socket object
+HOST = ARGS.ip            # Get local machine name
+PORT = ARGS.port              # Reserve a PORT for your service.
 
+S.connect((HOST, PORT))
+#S.close                     # Close the socket when done

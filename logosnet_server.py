2484bababe0c5b6e9389c0b74055ead2e2e43cad
yungl <yungl@wwu.edu>
2017-11-21 00:52:31 -0800

    Update logosnet_server.py

--- logosnet_server.py
+++ logosnet_server.py
@@ -2,6 +2,19 @@ import argparse
 import socket               # Import socket module
 import signal
 import struct
+import collections
+
+#object Participant
+class Participant(object):
+    def __init__(self, socketfd,username,message):
+        self.socketfd = socketfd
+        self.username = username
+        self.message = message
+
+participants = []
+
+for i in range(0, 255):
+    participants.append(Participant(-1," "," "))
 
 #argument parsing
 PARSER = argparse.ArgumentParser(
@@ -10,14 +23,27 @@ PARSER.add_argument("--port", type=int, metavar='p', help="port number")
 PARSER.add_argument("--ip", metavar='i', help="IP address for client")
 ARGS = PARSER.parse_args()
 
+#check duplicate usernam
+def checkDuplicate():
+    '''check username has been used or not'''
+    for i in range (0,255):
+        if participants[i].username == inputName:
+            return 0
+        else:
+            return 1
+    return 1
+
 #username method
 def get_username():
-    '''for inputing the username.'''
-    inputusername = input("Enter username, max 10 chars: ")
-    while len(inputusername) > 10 or inputusername.isalpha() == 0:
-        inputusername = input("Enter username, max 10 chars: ")
-    print(inputusername)
-    return inputusername
+    '''for inputing the username and check duplicate'''
+    inputName = input("Enter username, max 10 chars: ")
+    while len(inputName) > 10 or " " in inputName:
+        inputName = input("Enter username, max 10 chars: ")
+    print(inputName)
+    return inputName
+
+#check username exist or not
+
 
 
 # server socket setup and connection
